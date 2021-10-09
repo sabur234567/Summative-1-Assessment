27b07feeffee94f21f62922c6f53088b9124727d
sabur234567 <73675887+sabur234567@users.noreply.github.com>
2021-10-09 13:36:48 -0500

    Update WordController.java

--- src/main/java/com/company/controller/WordController.java
+++ src/main/java/com/company/controller/WordController.java
@@ -25,6 +25,12 @@ public class WordController {
         wordList.add(new Word(3215, "Coronagraph", "instrument for viewing the corona of the sun"));
         wordList.add(new Word(2723, "xerarch", "growing in dry places"));
         wordList.add(new Word(2151, "zendik", "heretic; magician"));
+        wordList.add(new Word(2361, "numinous", "of, like or pertaining to a deity; suffused with religious awe"));
+        wordList.add(new Word(1271, "jiggumbob", "a thingamabob; a gadget; a whatsit; a gewgaw"));
+        wordList.add(new Word(9525, "demulcent", "emulsifier; something soothing"));
+        wordList.add(new Word(6431, "macies", "atrophy"));
+        wordList.add(new Word(9251, "narial", "of, like or pertaining to the nostrils"));
+        wordList.add(new Word(3725, "pneumatophilosophy", "philosophy of spirits or the spirit world"));
     }
     @RequestMapping(value= "/word", method = RequestMethod.GET)
     @ResponseStatus(value= HttpStatus.OK)
