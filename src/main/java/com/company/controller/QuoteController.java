8e1571f9a1114f47c685ccff97c91e2a4d34a1b6
sabur234567 <73675887+sabur234567@users.noreply.github.com>
2021-10-09 13:41:03 -0500

    Update QuoteController.java

--- src/main/java/com/company/controller/QuoteController.java
+++ src/main/java/com/company/controller/QuoteController.java
@@ -21,8 +21,15 @@ public class QuoteController {
         quoteList = new ArrayList<>();
 
         quoteList.add(new Quote("YOLO", "Drake", 1234));
-        quoteList.add(new Quote("Test", "Testing123", 2345));
-        quoteList.add(new Quote("Test2", "Testing321", 2625));
+        quoteList.add(new Quote("Live your life", "Everybody", 2345));
+        quoteList.add(new Quote("Every human heart is human.", "Henry Wadsworth Longfellow", 2625));
+        quoteList.add(new Quote("Emulation is a noble and just passion, full of appreciation.", "J. C. F. von Schiller", 1624));
+        quoteList.add(new Quote("To teach is to learn twice.", "Joseph Joubert", 1631));
+        quoteList.add(new Quote("Persistent people begin their success where others end in failure.", "Edward Eggleston", 7326));
+        quoteList.add(new Quote("God hasn't called me to be successful. He's called me to be faithful.", "Mother Teresa", 4326));
+        quoteList.add(new Quote("If a free society cannot help the many who are poor, it cannot save the few who are rich.", "John F. Kennedy", 8331));
+        quoteList.add(new Quote("No man will ever bring out of the Presidency the reputation which carries him into it.", "Thomas Jefferson", 7423));
+        quoteList.add(new Quote("Not simply one of the virtues but the form of every virtue at the testing point, which means at the point of highest reality.", "C.S. Lewis", 6325));
     }
     @RequestMapping(value= "/quote", method = RequestMethod.GET)
     @ResponseStatus(value= HttpStatus.OK)
